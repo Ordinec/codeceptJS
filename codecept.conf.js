75366491b6152b621fcf4ea5c0643a3d15bad069
Serhii Moroz <sergiitst3@gmail.com>
2021-07-20 16:36:24 +0300

    Initial commit

---
+++ codecept.conf.js
@@ -0,0 +1,30 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js',
+    mainPage: './pages/main.js',
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'codeceptJS',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
